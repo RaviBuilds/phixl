985ffe497d355ae3adb2aa8090639f02c3bced19
Ravindra Kamble <write2ravi14@gmail.com>
2026-04-15 23:11:33 +0530

    done

--- components/LogoutBtn.tsx
+++ components/LogoutBtn.tsx
@@ -3,21 +3,21 @@ import logoutAction from "@/actions/logoutAction";
 import { useActionState } from "react";
 
 export default function LogoutBtn() {
+  // THE FIX: Remove the 'as string | null' casting from success
   const [state, onLogout, isPending] = useActionState(logoutAction, {
     error: null as string | null,
-    success: null as string | null,
+    success: null,
   });
+
   return (
-    <>
-      <form action={onLogout}>
-        <button
-          type="submit"
-          disabled={isPending}
-          className="hover:text-red-500 transition-colors text-left cursor-pointer"
-        >
-          {isPending ? " Logging out..." : "Logout"}
-        </button>
-      </form>
-    </>
+    <form action={onLogout}>
+      <button
+        type="submit"
+        disabled={isPending}
+        className="hover:text-red-500 transition-colors text-left cursor-pointer"
+      >
+        {isPending ? "Logging out..." : "Logout"}
+      </button>
+    </form>
   );
 }
